3ee5674b8c903d8ab823a95ee251e7fce8a910a6
Acurich <91753821+Acurich@users.noreply.github.com>
2021-10-29 18:46:45 +0300

    Update 1.9dop.java

--- module1/1.9dop.java
+++ module1/1.9dop.java
@@ -7,7 +7,30 @@
 //быть членами класса Geom;
 //иметь спецификатор доступа public;
 //быть статическими.
+public class Geom {
+       public static double segmentLength(long x_1, long y_1, long x_2, long y_2){
+        double d;
+        long i = Math.abs(x_1-x_2);
+        long j = Math.abs(y_1-y_2);
+        d = Math.sqrt(i*i+j*j);
+        return d;
+    }
+    public static double trianglePerimetr(long x_1, long y_1, long x_2, long y_2, long x_3, long y_3){
+        long i_1 = Math.abs(x_1-x_2);
+        long j_1 = Math.abs(y_1-y_2);
+        double d_1 = Math.sqrt(i_1*i_1+j_1*j_1);
+        long i_2 = Math.abs(x_2-x_3);
+        long j_2 = Math.abs(y_2-y_3);
+        double d_2 = Math.sqrt(i_2*i_2+j_2*j_2);
+        long i_3 = Math.abs(x_3-x_1);
+        long j_3 = Math.abs(y_3-y_1);
+        double d_3 = Math.sqrt(i_3*i_3+j_3*j_3);
+        double answer = d_1+d_2+d_3;
+        return answer;
 
+    }
+    
+}
 
 
 //Минимум 4 чисел
