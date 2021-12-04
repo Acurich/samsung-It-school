cfc9923cb4ef9fb8cf5826d364f800738d01a2c7
Acurich <91753821+Acurich@users.noreply.github.com>
2021-12-04 14:59:33 +0300

    Create 2.4vop.java

---
+++ module2/2.4vop.java
@@ -0,0 +1,10 @@
+1) 
+  Приложение еще не запущено ->
+
+[protected void onCreate()] -> [protected void onStart()] -> [protected void onResume()] ->
+
+Приложение Работает ->
+
+[protected void onPause()] -> [protected void onStop()] -> [protected void onDestroy()] ->
+
+Приложение уже не существует.
